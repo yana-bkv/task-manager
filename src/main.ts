d6c23e4af9bb5b11087f812f442b0f42e523eb5b
Jaana <jaana.boikova39@gmail.com>
2025-10-23 17:00:03 +0300

    feat: create and show all tasks on front

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,35 @@ import {LocalStorageRepository} from './repositories/localStorageRepository.js';
 
 const taskManagement = new LocalStorageRepository();
 
-taskManagement.delete(3);
+const addBtn = document.getElementById('addButton');
+const input = document.getElementById('taskTitle') as HTMLInputElement;
 
-console.log(taskManagement.findAll());
+// CREATE
+addBtn?.addEventListener('click', function(e) {
+    e.preventDefault();
+    const taskTitle :string  = input.value;
+    console.log(input, taskTitle);
+    taskManagement.create({id: 2, title: taskTitle});
+    input.value = '';
+})
+
+// SHOW ALL
+const tasksListDiv = document.getElementById('tasksList');
+const tasksStorage = taskManagement.findAll();
+
+
+tasksStorage.forEach(task=>{
+    const taskElement = document.createElement('div');
+    taskElement.classList.add('container-md');
+    taskElement.classList.add('mb-3');
+    taskElement.classList.add('task');
+
+    taskElement.textContent = task.title;
+    tasksListDiv?.append(taskElement)
+})
+
+// UPDATE
+
+
+// DELETE
+// taskManagement.delete(2);
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,7 +1,4 @@
 export interface Task {
     id: number;
     title: string;
-    description: string;
-    status: boolean;
-    created_at: Date;
 }
